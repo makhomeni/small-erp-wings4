88d5d383082f3778de886465a91b589b415c61db
hossaindoula@gmail.com <hossaindoula@gmail.com@a71b899a-115e-c629-6f87-0d2e2ee40419>
2012-07-26 21:01:52 +0000

    Domain created for inventory...

---
+++ App/grails-app/domain/com/jabait/scm/inventory/ProductClassification.groovy
@@ -0,0 +1,10 @@
+package com.jabait.scm.inventory
+
+class ProductClassification {
+    
+    String classification;// Bill of Materials, Purchased, Manufactured, Planned Item, Discontinued
+    String description;
+
+    static constraints = {
+    }
+}
