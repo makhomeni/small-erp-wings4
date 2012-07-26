387eb037033ac25cec9a9d3f1ac9713fd1c55a2e
shohag402@gmail.com <shohag402@gmail.com@a71b899a-115e-c629-6f87-0d2e2ee40419>
2012-07-26 19:36:14 +0000

    property added

--- trunk/App/grails-app/domain/com/jabait/scm/LocalVendor.groovy
+++ trunk/App/grails-app/domain/com/jabait/scm/LocalVendor.groovy
@@ -1,8 +1,19 @@
 package com.jabait.scm
 
+import com.jabait.hrm.Organization
+
 class LocalVendor {
 
     static constraints = {
     }
+
+    String firstName;
+    String lastName;
+    Organization organization;
+    String address;
+    String description;
+    String mobileNo;
+    String emailId;
+    String phoneNo;
 
 }
