2c7bab1282b9c6f736b4816dac115cf4ee30d55b
hossaindoula@gmail.com <hossaindoula@gmail.com@a71b899a-115e-c629-6f87-0d2e2ee40419>
2012-07-26 22:13:47 +0000

    product json data initiated in InventoryController...

--- App/grails-app/services/com/jabait/scm/inventory/InventoryService.groovy
+++ App/grails-app/services/com/jabait/scm/inventory/InventoryService.groovy
@@ -8,11 +8,18 @@ class InventoryService {
 
     List findAllProducts(){
         List<Product> products = Product.list();
-        List returnedProducts = new ArrayList();
-        Map<String,Object> productStorage = new HashMap<String,Object>();
+        List<Map<String,Object>> returnedProducts = new ArrayList<HashMap<String,Object>>();
+        Map<String,Object> productStorage;
         for (Product product : products) {
+            productStorage = new HashMap<String,Object>();
             productStorage.put("productName", product.productName);
+            productStorage.put("categoryName", product.productCategory.categoryName);
+            productStorage.put("classification", product.classification.classification);
+            productStorage.put("licenseInfo", product.licenseInfo);
+            productStorage.put("productType", product.productType.name);
+            returnedProducts.add(productStorage);
         }
-        return null;
+
+        return returnedProducts;
     }
 }
