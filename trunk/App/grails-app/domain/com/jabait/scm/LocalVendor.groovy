580ef7dd61987c61deded3a1453b2ba2bfb6760c
shohag402@gmail.com <shohag402@gmail.com@a71b899a-115e-c629-6f87-0d2e2ee40419>
2012-07-27 10:49:33 +0000

    in subversion someone uploaded empty file..................

--- trunk/App/grails-app/domain/com/jabait/scm/LocalVendor.groovy
+++ trunk/App/grails-app/domain/com/jabait/scm/LocalVendor.groovy
@@ -1,9 +1,34 @@
 package com.jabait.scm
 
+import com.jabait.hrm.Organization
+import com.jabait.scm.inventory.Product
+
 
 class LocalVendor extends Vendor {
 
+
+
+    String firstName;
+    String lastName;
+    Organization organization;
+    String address;
+    String description;
+    String mobileNo;
+    String emailId;
+    String phoneNo;
+
+    static hasMany = [products: Product];
+
     static constraints = {
+        firstName(nullable: true);
+        lastName(nullable: true);
+        organization(nullable: true);
+        address(nullable: true);
+        description(nullable: true);
+        mobileNo(nullable: true);
+        emailId(nullable: true);
+        phoneNo(nullable: true);
     }
+
 
 }
