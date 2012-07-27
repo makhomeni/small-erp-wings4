89f21cb907e9eda829596ea4af5d775f6b0ccb67
hossaindoula@gmail.com <hossaindoula@gmail.com@a71b899a-115e-c629-6f87-0d2e2ee40419>
2012-07-27 06:05:58 +0000

    JobOrder domain modified...

--- App/grails-app/domain/com/jabait/scm/JobOrder.groovy
+++ App/grails-app/domain/com/jabait/scm/JobOrder.groovy
@@ -2,6 +2,8 @@ package com.jabait.scm
 
 import com.jabait.security.User
 import com.jabait.scm.inventory.Product
+import com.jabait.scm.inventory.PriceList
+import com.jabait.hrm.Organization
 
 class JobOrder {
 
@@ -9,6 +11,12 @@ class JobOrder {
     Date createdDate;
     User createdBy;
     Product product;
+    Integer status; //1 draft, 2 Open, 3 Closed
+    //Warehouse warehouse; //to be added
+    Integer priority; //1 low, 2 medium, 3 high
+    PriceList priceList;
+    Integer paymentTerm; //1 immediate, 2
+    Organization organization;
 
     static mapping = {
         tablePerSubclass(true)
