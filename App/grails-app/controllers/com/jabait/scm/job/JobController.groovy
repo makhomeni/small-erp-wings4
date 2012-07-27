44439ad56d78d824fbee9f83770bdcdec787260b
hossaindoula@gmail.com <hossaindoula@gmail.com@a71b899a-115e-c629-6f87-0d2e2ee40419>
2012-07-27 06:16:41 +0000

    CRUD for JobController...

--- App/grails-app/controllers/com/jabait/scm/job/JobController.groovy
+++ App/grails-app/controllers/com/jabait/scm/job/JobController.groovy
@@ -3,4 +3,36 @@ package com.jabait.scm.job
 class JobController {
 
     def index() { }
+
+    def createPurchaseOrder(){
+        render(view: "");
+    }
+
+    def savePurchaseOrder(){
+        redirect(action: "createPurchaseOrder")
+    }
+
+    def purchaseOrderList(){
+        render(view: "");
+    }
+
+    def purchaseOrderJsonData(){
+        render();
+    }
+
+    def createSalesOrder(){
+        render(view: "");
+    }
+
+    def saveSalesOrder(){
+        redirect(action: "createSalesOrder")
+    }
+
+    def salesOrderList(){
+        render(view: "");
+    }
+
+    def salesOrderJsonData(){
+        render();
+    }
 }
