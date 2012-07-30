871899b550810f85861bfa4905657d7ce9f1512e
hossaindoula@gmail.com <hossaindoula@gmail.com@a71b899a-115e-c629-6f87-0d2e2ee40419>
2012-07-30 16:19:19 +0000

    StorageBin updated...

--- App/grails-app/domain/com/jabait/scm/inventory/StorageBin.groovy
+++ App/grails-app/domain/com/jabait/scm/inventory/StorageBin.groovy
@@ -8,8 +8,27 @@ class StorageBin {
 
     static belongsTo = [Warehouse];
 
-    StorageBin storageBin;
+    static hasMany = [products : Product];
+
+    String storageBinName;
+    Warehouse warehouse;
+    Double maximumWeight;
+    String shelvingSize;
+    Long numberOfShelves;
+    Long totalBins;
+    String model;
+    Double totalCapacity;
+    Integer storageBinType; // 1 small, 2 large pallets   //http://www.actionwp.com/storage-bins (for more info.)
+    //http://www.actionwp.com/spv-169.aspx (also)
+
 
     static constraints = {
+        storageBinName(unique: true);
+        warehouse(nullable: false);
+        maximumWeight(nullable: false);
+        totalCapacity(nullable: false);
+        totalBins(nullable: false);
+        numberOfShelves(nullable: false);
+        shelvingSize(nullable: false);
     }
 }
