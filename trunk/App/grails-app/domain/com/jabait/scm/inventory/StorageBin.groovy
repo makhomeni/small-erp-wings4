117e4a4570dfd62d3cb492e8d0be3b57efd93efe
hossaindoula@gmail.com <hossaindoula@gmail.com@a71b899a-115e-c629-6f87-0d2e2ee40419>
2012-07-30 12:32:09 +0000

    domains created...

---
+++ trunk/App/grails-app/domain/com/jabait/scm/inventory/StorageBin.groovy
@@ -0,0 +1,13 @@
+package com.jabait.scm.inventory
+
+import com.jabait.scm.Warehouse
+
+
+//The storage bin is the smallest available unit of space in a warehouse
+class StorageBin {
+
+    static belongsTo = [Warehouse];
+
+    static constraints = {
+    }
+}
