46fd380dccf2b71bdc82234b90d9038b03876e60
hossaindoula@gmail.com <hossaindoula@gmail.com@a71b899a-115e-c629-6f87-0d2e2ee40419>
2012-08-01 23:37:42 +0000

    Default ProductType added...

--- trunk/App/grails-app/domain/com/jabait/scm/inventory/ProductClassification.groovy
+++ trunk/App/grails-app/domain/com/jabait/scm/inventory/ProductClassification.groovy
@@ -9,10 +9,12 @@ class ProductClassification {
     }
 
     public static void initialize(){
-        new ProductClassification(classification: 'Bill of Material', description: 'Raw Material').save();
-        new ProductClassification(classification: 'Purchased', description: 'Purchased Item').save();
-        new ProductClassification(classification: 'Manufactured', description: 'Manufactured Item').save();
-        new ProductClassification(classification: 'Planned Item', description: 'Planned Item for MRP').save();
-        new ProductClassification(classification: 'Discontinued', description: 'Discontinued Item').save();
+        if (ProductClassification.count() == 0 ) {
+            new ProductClassification(classification: 'Bill of Material', description: 'Raw Material').save();
+            new ProductClassification(classification: 'Purchased', description: 'Purchased Item').save();
+            new ProductClassification(classification: 'Manufactured', description: 'Manufactured Item').save();
+            new ProductClassification(classification: 'Planned Item', description: 'Planned Item for MRP').save();
+            new ProductClassification(classification: 'Discontinued', description: 'Discontinued Item').save();
+        }
     }
 }
