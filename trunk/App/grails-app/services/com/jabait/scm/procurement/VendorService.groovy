b9865e69dbe29534da8df1f4cfe784c647deee54
shohag402@gmail.com <shohag402@gmail.com@a71b899a-115e-c629-6f87-0d2e2ee40419>
2012-08-01 09:41:14 +0000

    organization selection done.................................

--- trunk/App/grails-app/services/com/jabait/scm/procurement/VendorService.groovy
+++ trunk/App/grails-app/services/com/jabait/scm/procurement/VendorService.groovy
@@ -3,6 +3,7 @@ package com.jabait.scm.procurement
 import com.jabait.scm.Vendor
 import com.jabait.hrm.Organization
 import com.jabait.scm.LocalVendor
+import com.jabait.security.Address
 
 class VendorService {
 
@@ -13,14 +14,26 @@ class VendorService {
     def saveLocalVendor(params){
 
         LocalVendor localVendor = new LocalVendor();
+
         localVendor.firstName = params.firstName;
         localVendor.lastName = params.lastName;
         localVendor.description = params.description;
         localVendor.emailId = params.emailId;
+        localVendor.mobileNo = params.mobileNo;
         localVendor.phoneNo = params.phoneNo;
+
+        def country = params.country;
+        def streetAddress = params.address1;
+        def postalCode = params.postCode;
+
+        Address address = new Address();
+        address.streetAddress = streetAddress;
+        address.country = country;
+        address.postalCode = postalCode;
 
         Organization organization = Organization.get(Integer.parseInt(params.orgId));
         localVendor.organization = organization;
+        localVendor.billingAddress = address;
         if(localVendor.save()){
             return "Vendor created successfully";
         }else{
