16d44bfcc3584b592e5e07e76f3480aedd854eac
hossaindoula@gmail.com <hossaindoula@gmail.com@a71b899a-115e-c629-6f87-0d2e2ee40419>
2012-08-02 16:27:03 +0000

    added hasMany relationship for Product in PurchaseOrder and SalesOrder...

--- App/grails-app/domain/com/jabait/scm/PurchaseOrder.groovy
+++ App/grails-app/domain/com/jabait/scm/PurchaseOrder.groovy
@@ -2,12 +2,15 @@ package com.jabait.scm
 
 import com.jabait.security.Address
 import com.jabait.scm.inventory.ShippingMethod
+import com.jabait.scm.inventory.Product
 
 class PurchaseOrder extends JobOrder {
 
     Vendor vendor;
     Address shippingAddress;
     ShippingMethod shippingMethod;
+
+    static hasMany = [products : Product]
 
     static mapping = {
         table("purchase_order")
--- App/grails-app/domain/com/jabait/scm/SalesOrder.groovy
+++ App/grails-app/domain/com/jabait/scm/SalesOrder.groovy
@@ -2,12 +2,15 @@ package com.jabait.scm
 
 import com.jabait.security.Address
 import com.jabait.scm.inventory.ShippingMethod
+import com.jabait.scm.inventory.Product
 
 class SalesOrder extends JobOrder {
 
     Customer customer;
     Address shippingAddress;
     ShippingMethod shippingMethod;
+
+    static hasMany = [products : Product]
 
     static mapping = {
         table("sales_order")
