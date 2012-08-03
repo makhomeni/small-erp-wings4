35215f83941429f6584284ff34b127ea1dbc01a7
hossaindoula@gmail.com <hossaindoula@gmail.com@a71b899a-115e-c629-6f87-0d2e2ee40419>
2012-08-03 19:55:43 +0000

    CategoryResource's create category added...

--- App/grails-app/resources/com/jabait/scm/inventory/CategoryResource.groovy
+++ App/grails-app/resources/com/jabait/scm/inventory/CategoryResource.groovy
@@ -4,8 +4,12 @@ import javax.ws.rs.Path
 import javax.ws.rs.Produces
 import javax.ws.rs.core.Response
 import javax.ws.rs.PathParam
-import static org.grails.jaxrs.response.Responses.ok
+import static org.grails.jaxrs.response.Responses.*
 import javax.ws.rs.GET
+import org.json.JSONObject
+import javax.ws.rs.POST
+import javax.ws.rs.core.MediaType
+import javax.ws.rs.Consumes
 
 /**
  * Created by IntelliJ IDEA.
@@ -31,5 +35,19 @@ class CategoryResource {
     @Produces(["application/xml"])
     Response readAll(){
         ok inventoryService.readAllCategories();
+    }
+
+    @POST
+    @Produces([MediaType.APPLICATION_JSON])
+    @Consumes([MediaType.APPLICATION_JSON])
+    Response create(Object category){
+        
+        JSONObject categoryJsonObject = new JSONObject(category);
+        Category parentCategory = Category.get(Long.valueOf(
+                categoryJsonObject.get("parentCategory").toString()));
+        Category categoryDto = new Category();
+        categoryDto.categoryName = categoryJsonObject.get("categoryName").toString();
+        categoryDto.parentCategory = parentCategory;
+        created categoryDto.save();
     }
 }
