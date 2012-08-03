d5fb2d290ab28fe4aee1d3f9c6d230a2a032f60e
hossaindoula@gmail.com <hossaindoula@gmail.com@a71b899a-115e-c629-6f87-0d2e2ee40419>
2012-08-03 20:47:30 +0000

    RESTFeed added...

--- trunk/Desktop/Inventory/src/com/wings4/client/Inventory.java
+++ trunk/Desktop/Inventory/src/com/wings4/client/Inventory.java
@@ -2,7 +2,7 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.wings4;
+package com.wings4.client;
 
 /**
  *
---
+++ trunk/Desktop/Inventory/src/com/wings4/util/InventoryConstants.java
@@ -0,0 +1,24 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.wings4.util;
+
+/**
+ *
+ * @author ronnie
+ */
+public class InventoryConstants {
+    
+    public static final String MEDIA_JSON = "application/json";
+    public static final String MEDIA_XML = "application/xml";
+    public static final String MEDIA_PLAIN = "plain/text";
+    
+    public static final String GET = "GET";
+    public static final String POST = "POST";
+    public static final String PUT = "PUT";
+    public static final String DELETE = "DELETE";
+    
+    public static final String appName = "App";
+    
+}
