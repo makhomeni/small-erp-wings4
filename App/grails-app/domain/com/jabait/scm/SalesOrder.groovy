dd42c149c90199faae823544a9738c39cf955165
shohag402@gmail.com <shohag402@gmail.com@a71b899a-115e-c629-6f87-0d2e2ee40419>
2012-08-06 19:07:29 +0000

    created by ronnie vai..................

--- App/grails-app/domain/com/jabait/scm/SalesOrder.groovy
+++ App/grails-app/domain/com/jabait/scm/SalesOrder.groovy
@@ -3,14 +3,19 @@ package com.jabait.scm
 import com.jabait.security.Address
 import com.jabait.scm.inventory.ShippingMethod
 import com.jabait.scm.inventory.Product
+import com.jabait.accounting.PaymentTerm
+import com.jabait.scm.inventory.PriceList
+import com.jabait.scm.inventory.InventoryRegister
 
 class SalesOrder extends JobOrder {
 
     Customer customer;
     Address shippingAddress;
     ShippingMethod shippingMethod;
+    PaymentTerm paymentTerm;
+    PriceList priceList;
 
-    static hasMany = [products : Product]
+    static hasMany = [inventories : InventoryRegister]
 
     static mapping = {
         table("sales_order")
