f520afd2fd8e761df0bdd07e958aad20b074f9b6
hossaindoula@gmail.com <hossaindoula@gmail.com@a71b899a-115e-c629-6f87-0d2e2ee40419>
2012-08-09 21:03:36 +0000

    ProductResource updated...

--- App/grails-app/resources/com/jabait/scm/inventory/ProductResource.groovy
+++ App/grails-app/resources/com/jabait/scm/inventory/ProductResource.groovy
@@ -14,6 +14,8 @@ import javax.ws.rs.Path
 import javax.ws.rs.PathParam
 import org.json.JSONObject
 import javax.ws.rs.POST
+import javax.ws.rs.core.MediaType
+import com.jabait.util.UnitOfMeasure
 
 @Path("/api/product")
 class ProductResource {
@@ -23,11 +25,21 @@ class ProductResource {
 
     @POST
     @Consumes(["application/json"])
-    @Produces(["application/xml"])
-    Response create(Object product){
+    @Produces(["application/json"])
+    Response createProduct(String product){
         JSONObject productSave = new JSONObject(product);
         Product productObject = new Product();
         productObject.productName = productSave.get("productName");
+        productObject.productCategory = Category.get(Integer.parseInt(productSave.get("productName").toString()));
+        productObject.stockKeepingUnit = productSave.get("stockKeepingUnit");
+        productObject.unitOfMeasure = UnitOfMeasure.get(Integer.parseInt(
+                productSave.get("unitOfMeasure").toString()));
+        productObject.productType = ProductType.get(Integer.parseInt(productSave.get("productName").toString()));
+        productObject.universalProductCode = productSave.get("universalProductCode");
+        productObject.licenseInfo = productSave.get("licenseInfo");
+        productObject.classification = ProductClassification.get(Integer.parseInt(
+                productSave.get("universalProductCode").toString()));
+
         ok inventoryService.save(productObject);
     }
 
