d9af94c1867946b939cdc12ff4e6a13fde5587da
wings4.masum@gmail.com <wings4.masum@gmail.com@a71b899a-115e-c629-6f87-0d2e2ee40419>
2012-08-09 19:37:03 +0000

    CustomerResource.......................

---
+++ App/grails-app/resources/com/jabait/scm/CustomerResource.groovy
@@ -0,0 +1,40 @@
+package com.jabait.scm
+
+import static org.grails.jaxrs.response.Responses.*
+
+import javax.ws.rs.Consumes
+import javax.ws.rs.DELETE
+import javax.ws.rs.GET
+import javax.ws.rs.Produces
+import javax.ws.rs.PUT
+import javax.ws.rs.core.Response
+
+import org.grails.jaxrs.provider.DomainObjectNotFoundException
+import javax.ws.rs.Path
+
+@Path('/api/customer')
+@Consumes(['application/xml','application/json'])
+@Produces(['application/xml','application/json'])
+class CustomerResource {
+    
+    def customerResourceService
+    def id
+    
+    @GET
+    Response read() {
+        ok customerResourceService.read(id)
+    }
+    
+    @PUT
+    Response update(Customer dto) {
+        dto.id = id
+        ok customerResourceService.update(dto)
+    }
+    
+    @DELETE
+    void delete() {
+        customerResourceService.delete(id)
+    }
+    
+}
+
