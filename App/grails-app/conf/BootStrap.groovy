fdc94c8625c0e11640c82b3833e164aa4cf30173
shohag402@gmail.com <shohag402@gmail.com@a71b899a-115e-c629-6f87-0d2e2ee40419>
2012-08-11 10:33:46 +0000

    payment term and delivery term added..................

--- App/grails-app/conf/BootStrap.groovy
+++ App/grails-app/conf/BootStrap.groovy
@@ -19,6 +19,7 @@ class BootStrap {
 
         User.initialize();
         Feature.initialize();
+        Feature.initializeSalesOrder();
         Organization.initialize();
         Department.initialize();
         CalendarType.initialize();
