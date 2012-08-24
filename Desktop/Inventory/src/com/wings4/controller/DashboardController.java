5d608bd12411e271830e985526dbb70a3f37f6c4
hossaindoula@gmail.com <hossaindoula@gmail.com@a71b899a-115e-c629-6f87-0d2e2ee40419>
2012-08-24 22:06:03 +0000

    Refactored...

---
+++ Desktop/Inventory/src/com/wings4/controller/DashboardController.java
@@ -0,0 +1,11 @@
+package com.wings4.controller;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: ronnie
+ * Date: 8/24/12
+ * Time: 5:22 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class DashboardController {
+}
