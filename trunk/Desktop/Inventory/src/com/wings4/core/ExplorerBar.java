395857807933d226bdfbf9cfc32990396eaf2341
hossaindoula@gmail.com <hossaindoula@gmail.com@a71b899a-115e-c629-6f87-0d2e2ee40419>
2012-08-27 16:43:16 +0000

    Refactored Client package name to Core and shifted the core related GUI oriented stuffs under respective packages...

---
+++ trunk/Desktop/Inventory/src/com/wings4/core/ExplorerBar.java
@@ -0,0 +1,39 @@
+package com.wings4.core;
+
+import com.wings4.core.outlook.GeneralControlOutlook;
+import com.wings4.core.outlook.MaterialsControlOutlook;
+import com.wings4.util.IconFactory;
+import com.nepxion.swing.outlookbar.JFlatOutlookBar;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: ronnie
+ * Date: 8/18/12
+ * Time: 10:05 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class ExplorerBar extends JFlatOutlookBar {
+
+    private static ExplorerBar explorerBar;
+
+    public static ExplorerBar getInstance(){
+        if(explorerBar == null){
+            explorerBar = new ExplorerBar();
+        }
+
+        return explorerBar;
+    }
+
+    private ExplorerBar() {
+        addTab("General", IconFactory.getSwingIcon("ggg.png"),
+                makeScrollPane(new GeneralControlOutlook()), "General");
+        addTab("Materials", IconFactory.getSwingIcon("component/button_16.png"),
+                makeScrollPane(new MaterialsControlOutlook()), "Materials");
+        addTab("Sales", IconFactory.getSwingIcon("component/button_16.png"),
+                makeScrollPane(new GeneralControlOutlook()), "Sales");
+        addTab("Purchasing", IconFactory.getSwingIcon("component/button_16.png"),
+                makeScrollPane(new GeneralControlOutlook()), "Purchasing");
+        addTab("Manufacturing", IconFactory.getSwingIcon("component/button_16.png"),
+                makeScrollPane(new GeneralControlOutlook()), "Manufacturing");
+    }
+}
