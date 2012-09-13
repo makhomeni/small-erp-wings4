b6313846192df5dad6bf26b464135c5f49f636a4
shohag402@gmail.com <shohag402@gmail.com@a71b899a-115e-c629-6f87-0d2e2ee40419>
2012-09-13 11:31:13 +0000

    grails version....

--- App/grails-app/domain/com/jabait/scm/SalesOrder.groovy
+++ App/grails-app/domain/com/jabait/scm/SalesOrder.groovy
@@ -8,7 +8,8 @@ import com.jabait.scm.inventory.PriceList
 import com.jabait.scm.inventory.InventoryRegister
 
 class SalesOrder extends JobOrder {
-
+    
+    Integer id;
     Customer customer;
     String address1;
     String address2;
