e33e0c7c3a451c20b647c61085826270080a5108
shohag402@gmail.com <shohag402@gmail.com@a71b899a-115e-c629-6f87-0d2e2ee40419>
2012-09-19 12:34:49 +0000

    grails version....

--- trunk/App/grails-app/resources/com/jabait/scm/PurchaseOrderResource.groovy
+++ trunk/App/grails-app/resources/com/jabait/scm/PurchaseOrderResource.groovy
@@ -11,6 +11,12 @@ import javax.ws.rs.core.Response
 
 import org.grails.jaxrs.provider.DomainObjectNotFoundException
 import javax.ws.rs.Path
+import javax.ws.rs.POST
+import org.json.JSONObject
+import com.jabait.hrm.Organization
+import com.jabait.accounting.PaymentTerm
+import com.jabait.security.User
+import com.jabait.scm.inventory.ShippingMethod
 
 @Path('/api/purchaseOrder')
 @Consumes(['application/xml', 'application/json'])
@@ -34,6 +40,39 @@ class PurchaseOrderResource {
     @DELETE
     void delete() {
         purchaseOrderResourceService.delete(id)
+    }
+
+    @POST
+    Response create(String purchaseOrderJson){
+
+        JSONObject jsonObject = new JSONObject(purchaseOrderJson);
+        PurchaseOrder purchaseOrder = new PurchaseOrder();
+        purchaseOrder.organization = Organization.get(1);
+        purchaseOrder.paymentTerm = PaymentTerm.get(1);
+        purchaseOrder.shippingAddress = "sdsdsd";
+        purchaseOrder.vendor = Vendor.get(1);
+        purchaseOrder.createdBy = User.get(1);
+        purchaseOrder.createdDate = new Date();
+        purchaseOrder.deliveryTerm = DeliveryTerm.get(1);
+        purchaseOrder.dueDate = new Date();
+        purchaseOrder.isArchived = false;
+        purchaseOrder.isSent = false;
+        purchaseOrder.jobName = "purchase order";
+        purchaseOrder.orderQuantity = 1;
+        purchaseOrder.priority = 1;
+        purchaseOrder.shippingMethod = ShippingMethod.get(1);
+        purchaseOrder.status = 1;
+        if (purchaseOrder.save()) {
+            println "saved";
+        } else {
+            purchaseOrder.errors.each {
+                println it;
+            }
+            println "not saved";
+        }
+        String organizationName = jsonObject.get("organizationId");
+        println "organizationName = $organizationName"
+
     }
 
 }
