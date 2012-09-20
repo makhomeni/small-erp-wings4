58d4707bcffaaa602c1fe8ea31d6f8f9424d241f
shohag402@gmail.com <shohag402@gmail.com@a71b899a-115e-c629-6f87-0d2e2ee40419>
2012-09-20 08:05:45 +0000

    grails version....

---
+++ App/grails-app/domain/com/jabait/scm/Sales.groovy
@@ -0,0 +1,15 @@
+package com.jabait.scm
+
+import com.jabait.scm.inventory.Product
+
+class Sales {
+    Customer customer;
+    Product product;
+    Date salesDate;
+    Double price;
+    Integer quantity;
+    String salesOrder;
+
+    static constraints = {
+    }
+}
