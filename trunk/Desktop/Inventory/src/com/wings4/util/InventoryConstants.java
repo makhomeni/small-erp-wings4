6def0ac9e94f6de7d67eedf62ee0d6b68f8992f3
hossaindoula@gmail.com <hossaindoula@gmail.com@a71b899a-115e-c629-6f87-0d2e2ee40419>
2012-09-23 06:40:09 +0000

    Refactored...

--- trunk/Desktop/Inventory/src/com/wings4/util/InventoryConstants.java
+++ trunk/Desktop/Inventory/src/com/wings4/util/InventoryConstants.java
@@ -19,12 +19,17 @@ public interface InventoryConstants {
     public static final String PUT = "PUT";
     public static final String DELETE = "DELETE";
     
-    public static final String appName = "App";
+    public static final String CONTEXT_PATH = "App";
+    public static final String EXTRA_PATH = "/api/";
     
     public static final String serverAddressPrefix = "http://";
 
     public static final String resourceDirectory = "/com/wings4/resource/";
 
     public static final String RESOURCE_PATH = "com/wings4/resource/";
+    
+    public static final String LOGIN_ERROR = "Login Error";
+    public static final String INVALID_URL_MESSAGE = "The URL Provided is incorrect";
+    public static final String BAD_CREDENTIAL_MESSAGE = "Bad Username/Password combination provided";
 
 }
