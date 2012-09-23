98580742c1c5cd88c928d0ae5147120c29d17d91
hossaindoula@gmail.com <hossaindoula@gmail.com@a71b899a-115e-c629-6f87-0d2e2ee40419>
2012-09-23 10:59:55 +0000

    Panels' Constant values put in the InventoryConstants...

--- Desktop/Inventory/src/com/wings4/core/panel/ExternalVendorButtonPanel.java
+++ Desktop/Inventory/src/com/wings4/core/panel/ExternalVendorButtonPanel.java
@@ -1,13 +1,18 @@
 package com.wings4.core.panel;
 
+import com.nepxion.swing.action.JSecurityAction;
 import com.nepxion.swing.border.BorderManager;
 import com.nepxion.swing.layout.filed.FiledLayout;
 import com.towel.el.annotation.AnnotationResolver;
 import com.towel.swing.table.ObjectTableModel;
+import com.wings4.core.toggle.ExternalVendorCreateTogglePanel;
+import com.wings4.core.toggle.GeneralToggleActionButton;
 import com.wings4.model.ExternalVendor;
 import com.wings4.model.LocalVendor;
+import com.wings4.util.InventoryConstants;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -22,6 +27,9 @@ public class ExternalVendorButtonPanel extends JPanel {
 
     private JScrollPane vendorListHolder;
     private JTable vendorTable;
+    private JToolBar localVendorToolBar;
+    private JButton createLocalVendorButton;
+    private JButton reportLocalVendorButton;
 
     public ExternalVendorButtonPanel() {
         setLayout(new FiledLayout(FiledLayout.COLUMN, FiledLayout.FULL, 0));
@@ -37,6 +45,34 @@ public class ExternalVendorButtonPanel extends JPanel {
 
             vendorListHolder = new JScrollPane();
             vendorTable = new JTable();
+
+            vendorListHolder = new JScrollPane();
+            vendorTable = new JTable();
+
+            createLocalVendorButton.setIcon(new ImageIcon(getClass().
+                    getResource(InventoryConstants.resourceDirectory.
+                            concat("list-add.png"))));
+            createLocalVendorButton.setFocusable(false);
+            createLocalVendorButton.setHorizontalTextPosition(SwingConstants.CENTER);
+            createLocalVendorButton.setVerticalTextPosition(SwingConstants.BOTTOM);
+            localVendorToolBar.add(createLocalVendorButton);
+
+            createLocalVendorButton.addActionListener(new JSecurityAction() {
+                @Override
+                public void execute(ActionEvent actionEvent) {
+                    GeneralToggleActionButton productCreateButton = new GeneralToggleActionButton(new
+                            ExternalVendorCreateTogglePanel());
+                    productCreateButton.doClick();
+                }
+            });
+
+            reportLocalVendorButton.setIcon(new ImageIcon(getClass().
+                    getResource(InventoryConstants.resourceDirectory.
+                            concat("document-print.png"))));
+            reportLocalVendorButton.setFocusable(false);
+            reportLocalVendorButton.setHorizontalTextPosition(SwingConstants.CENTER);
+            reportLocalVendorButton.setVerticalTextPosition(SwingConstants.BOTTOM);
+            localVendorToolBar.add(reportLocalVendorButton);
 
             AnnotationResolver resolver = new AnnotationResolver(ExternalVendor.class);
             final ObjectTableModel<ExternalVendor> tableModel = new ObjectTableModel<ExternalVendor>(
