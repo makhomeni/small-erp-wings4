e90140858315b24708bfab0610892205b0b8858d
hossaindoula@gmail.com <hossaindoula@gmail.com@a71b899a-115e-c629-6f87-0d2e2ee40419>
2012-09-23 06:11:07 +0000

    Refactored...

---
+++ Desktop/Inventory/src/com/wings4/core/panel/SalesCreateButtonPanel.java
@@ -0,0 +1,20 @@
+package com.wings4.core.panel;
+
+import javax.swing.*;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: ronnie
+ * Date: 9/23/12
+ * Time: 9:57 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class SalesCreateButtonPanel extends JPanel {
+
+    public SalesCreateButtonPanel() {
+    }
+
+    public class SalesCreatePanel extends JPanel {
+
+    }
+}
