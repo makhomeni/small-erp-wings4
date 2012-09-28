1f239b8a9b5ebff37de09a224effdd108908a30f
dabluhossain@gmail.com <dabluhossain@gmail.com@a71b899a-115e-c629-6f87-0d2e2ee40419>
2012-09-28 09:03:32 +0000

    ffg

---
+++ Desktop/Inventory/src/com/wings4/core/panel/SalesCreatePanel.java
@@ -0,0 +1,168 @@
+package com.wings4.core.panel;
+
+import com.jgoodies.forms.builder.DefaultFormBuilder;
+import com.jgoodies.forms.layout.FormLayout;
+import com.jidesoft.combobox.TableComboBox;
+import com.jidesoft.dialog.JideOptionPane;
+import com.nepxion.swing.action.JSecurityAction;
+import com.nepxion.swing.layout.filed.FiledLayout;
+import com.towel.el.annotation.AnnotationResolver;
+import com.towel.swing.table.ObjectTableModel;
+import com.wings4.dao.CommonDao;
+import com.wings4.dao.JobDao;
+import com.wings4.model.*;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: MASUM
+ * Date: 9/28/12
+ * Time: 12:00 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class SalesCreatePanel   extends JPanel {
+
+    public SalesCreatePanel() {
+        setLayout(new FiledLayout(FiledLayout.COLUMN, FiledLayout.FULL, 0));
+        setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5));
+        add(new SalesCreate());
+    }
+
+    public class SalesCreate extends JPanel {
+        public SalesCreate() {
+            DefaultFormBuilder builder = new DefaultFormBuilder(new FormLayout(""));
+            builder.setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5));
+            builder.appendColumn("right:pref");
+            builder.appendColumn("3dlu");
+            builder.appendColumn("fill:max(pref; 100px)");
+            builder.appendColumn("5dlu");
+            builder.appendColumn("right:pref");
+            builder.appendColumn("3dlu");
+            builder.appendColumn("fill:max(pref; 10px)");
+
+
+
+
+            AnnotationResolver customerResolver = new AnnotationResolver( Customer.class);
+            AnnotationResolver productResolver = new AnnotationResolver(Product.class);
+
+
+
+
+
+
+            final ObjectTableModel<Customer> tableModelCustomer = new ObjectTableModel< Customer>(
+                    customerResolver, "id,firstName,lastName,emailId,organization,mobileNumber,phoneNumber,address,contact,reference,billingAddress");
+
+            final ObjectTableModel<Product> tableModelProduct = new ObjectTableModel<Product>(
+                    productResolver, "productId,productName");
+
+
+
+            tableModelCustomer.setData(CommonDao.findAllCustomers());
+            tableModelProduct.setData(CommonDao.findAllProducts());
+            //tableModel.setData(MaterialDao.findAllCategories());
+
+
+
+            final JTextField priceText = new JTextField();
+            final JTextField quantityText = new JTextField();
+            final JTextField salesOrderText = new JTextField();
+
+
+
+            final TableComboBox productCombo = new TableComboBox(tableModelProduct);
+            final TableComboBox customerCombo = new TableComboBox(tableModelCustomer);
+
+
+
+
+            JButton submitSales = new JButton();
+            JButton cancelSales = new JButton();
+
+            builder.append("Customer:",customerCombo);
+            builder.nextLine();
+            builder.append("Product :",productCombo);
+            builder.nextLine();
+            builder.append("Price :", priceText);
+            builder.nextLine();
+            builder.append("Quantity :",quantityText);
+            builder.nextLine();
+            builder.append("Sales :",salesOrderText);
+            builder.nextLine();
+
+
+
+
+            submitSales.setText("Submit");
+            cancelSales.setText("Cancel");
+
+            builder.append(submitSales);
+            builder.append(cancelSales);
+
+
+            add(builder.getPanel());
+
+            submitSales.addActionListener(new JSecurityAction() {
+                @Override
+                public void execute(ActionEvent actionEvent) {
+                    Sales sales = new Sales();
+                    sales.setCustomerName(customerCombo.getSelectedItem().toString());
+                    sales.setProductName(productCombo.getSelectedItem().toString());
+                    sales.setPrice(Double.parseDouble(priceText.getText()));
+                    sales.setQuantity(Integer.parseInt(quantityText.getText()));
+                    if (JobDao.saveSales(sales)) {
+                        JideOptionPane.showMessageDialog(null, "Purchase Order Saved Successfully", "Success",
+                                JOptionPane.INFORMATION_MESSAGE);
+                    } else {
+                        JideOptionPane.showMessageDialog(null, "Purchase Order Saved Failed", "Failed",
+                                JOptionPane.ERROR_MESSAGE);
+                    }
+//                    PurchaseOrder purchaseOrder = new PurchaseOrder();
+//
+//                    System.out.println(organizationCombo.getSelectedItem().toString());
+//
+//                    purchaseOrder.setOrganizationId(organizationCombo.getSelectedItem().toString());
+//                    purchaseOrder.setPaymentTermId(paymentTermCombo.getSelectedItem().toString());
+//                    purchaseOrder.setShippingAddress(shippingAddressText.getText());
+//                    purchaseOrder.setShippingMethodId(shippingMethodCombo.getSelectedItem().toString());
+//                    purchaseOrder.setVendorId(vendorCombo.getSelectedItem().toString());
+//                    purchaseOrder.setProductId(productCombo.getSelectedItem().toString());
+//                    purchaseOrder.setDeliveryTermId(deliveryTermCombo.getSelectedItem().toString());
+//                    purchaseOrder.setOrderQuantity(orderQuantity.getText());
+//                    purchaseOrder.setJobName(jobNameTest.getText());
+//
+//                    if (JobDao.savePurchaseOrder(purchaseOrder)) {
+//                        JideOptionPane.showMessageDialog(null, "Purchase Order Saved Successfully", "Success",
+//                                JOptionPane.INFORMATION_MESSAGE);
+//                    } else {
+//                        JideOptionPane.showMessageDialog(null, "Purchase Order Saved Failed", "Failed",
+//                                JOptionPane.ERROR_MESSAGE);
+//                    }
+                }
+            });
+//            submitCategory.addActionListener(new JSecurityAction() {
+//                @Override
+//                public void execute(ActionEvent actionEvent) {
+//                    Category category = new Category();
+//                    category.setCategoryName(categoryNameText.getText());
+////                    try {
+////                        category.setParentCategory(parentCategory.getSelectedItem().toString());
+////                    } catch (Exception ex) {
+////                        ex.printStackTrace();
+////                    }
+//
+//                    if(JobDao.saveSalesOrder(new SalesOrder()))
+//                        JideOptionPane.showMessageDialog(null, "Category Saved Successfully", "Success",
+//                                JOptionPane.INFORMATION_MESSAGE);
+//                    else
+//                        JideOptionPane.showMessageDialog(null, "Category Saved Failed", "Failed",
+//                                JOptionPane.ERROR_MESSAGE);
+//                }
+//            });
+        }
+    }
+
+}
