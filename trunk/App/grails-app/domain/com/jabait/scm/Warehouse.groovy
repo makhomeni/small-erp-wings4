e7d3a7dde30b23f1c5713581d8506f90e39ffa6e
shohag402@gmail.com <shohag402@gmail.com@a71b899a-115e-c629-6f87-0d2e2ee40419>
2012-09-29 04:42:48 +0000

    grails version....

--- trunk/App/grails-app/domain/com/jabait/scm/Warehouse.groovy
+++ trunk/App/grails-app/domain/com/jabait/scm/Warehouse.groovy
@@ -9,10 +9,20 @@ class Warehouse {
     
     Organization organization;
     String wareHouseName;
-    Address address;
+    String address;
 
     static hasMany = [storageBins : StorageBin]
 
     static constraints = {
+    }
+
+    public static void initialize(){
+        if(Warehouse.getCount() == 0){
+            Warehouse warehouse = new Warehouse();
+            warehouse.organization = Organization.get(1);
+            warehouse.wareHouseName = "Mohakhali Warehouse";
+            warehouse.address = "Mohakhali DOHS";
+            warehouse.save();
+        }
     }
 }
