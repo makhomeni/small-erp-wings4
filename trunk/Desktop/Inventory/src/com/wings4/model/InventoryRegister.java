c559a336ec216b0e6a6abbe93a1a6e975a410529
hossaindoula@gmail.com <hossaindoula@gmail.com@a71b899a-115e-c629-6f87-0d2e2ee40419>
2012-10-13 11:50:55 +0000

    InventoryButtonPanel and MaterialDao committed...

--- trunk/Desktop/Inventory/src/com/wings4/model/InventoryRegister.java
+++ trunk/Desktop/Inventory/src/com/wings4/model/InventoryRegister.java
@@ -2,6 +2,8 @@ package com.wings4.model;
 
 import com.towel.el.annotation.Resolvable;
 
+import java.util.Date;
+
 /**
  * Created by IntelliJ IDEA.
  * User: ronnie
@@ -17,32 +19,34 @@ public class InventoryRegister {
     @Resolvable(colName = "Product Name")
     private String productName;
 
-    @Resolvable(colName = "On Hand")
-    private Integer onHand;
+    private Double onHand;
 
-    @Resolvable(colName = "On Order")
-    private Integer onOrder;
+    private Double onHandFromLocalSupplier;
 
-    @Resolvable(colName = "Allocated")
-    private Integer allocated;
+    private Double onHandImportByLC;
 
-    @Resolvable(colName = "Committed")
-    private Integer committed;
+    private Double onSalesReturn;
 
-    @Resolvable(colName = "Unavailable")
-    private Integer unavailable;
+    private Double onLoanReturn;
 
-    @Resolvable(colName = "Back Ordered")
-    private Integer backOrdered;
+    private Double onPurchaseOrder = 0.0;//how many product is ordered will be added
+    private Double onSalesOrder = 0.0;
+    private Double allocated = 0.0;
 
-    @Resolvable(colName = "Drop Ship")
-    private Integer dropShip;
+    private Double committed = 0.0;
 
-    @Resolvable(colName = "Available for Sale")
-    private Integer availableForSale;
+    private Double unavailable = 0.0;
+    private Double unavailableFromSales = 0.0;
+    private Double unavailableFromSample = 0.0;
+    private Double unavailableFromLoan = 0.0;
 
-    @Resolvable(colName = "Available to Pick")
-    private Integer availableToPick;
+    private Double backOrdered = 0.0;
+    private Double dropShip = 0.0;
+
+    private Double availableForSale = 0.0;
+    private Double availableToPick = 0.0;
+
+    private String modifiedDate;
 
     public Integer getId() {
         return id;
@@ -60,75 +64,147 @@ public class InventoryRegister {
         this.productName = productName;
     }
 
-    public Integer getOnHand() {
+    public Double getOnHand() {
         return onHand;
     }
 
-    public void setOnHand(Integer onHand) {
+    public void setOnHand(Double onHand) {
         this.onHand = onHand;
     }
 
-    public Integer getOnOrder() {
-        return onOrder;
+    public Double getOnHandFromLocalSupplier() {
+        return onHandFromLocalSupplier;
     }
 
-    public void setOnOrder(Integer onOrder) {
-        this.onOrder = onOrder;
+    public void setOnHandFromLocalSupplier(Double onHandFromLocalSupplier) {
+        this.onHandFromLocalSupplier = onHandFromLocalSupplier;
     }
 
-    public Integer getAllocated() {
+    public Double getOnHandImportByLC() {
+        return onHandImportByLC;
+    }
+
+    public void setOnHandImportByLC(Double onHandImportByLC) {
+        this.onHandImportByLC = onHandImportByLC;
+    }
+
+    public Double getOnSalesReturn() {
+        return onSalesReturn;
+    }
+
+    public void setOnSalesReturn(Double onSalesReturn) {
+        this.onSalesReturn = onSalesReturn;
+    }
+
+    public Double getOnLoanReturn() {
+        return onLoanReturn;
+    }
+
+    public void setOnLoanReturn(Double onLoanReturn) {
+        this.onLoanReturn = onLoanReturn;
+    }
+
+    public Double getOnPurchaseOrder() {
+        return onPurchaseOrder;
+    }
+
+    public void setOnPurchaseOrder(Double onPurchaseOrder) {
+        this.onPurchaseOrder = onPurchaseOrder;
+    }
+
+    public Double getOnSalesOrder() {
+        return onSalesOrder;
+    }
+
+    public void setOnSalesOrder(Double onSalesOrder) {
+        this.onSalesOrder = onSalesOrder;
+    }
+
+    public Double getAllocated() {
         return allocated;
     }
 
-    public void setAllocated(Integer allocated) {
+    public void setAllocated(Double allocated) {
         this.allocated = allocated;
     }
 
-    public Integer getCommitted() {
+    public Double getCommitted() {
         return committed;
     }
 
-    public void setCommitted(Integer committed) {
+    public void setCommitted(Double committed) {
         this.committed = committed;
     }
 
-    public Integer getUnavailable() {
+    public Double getUnavailable() {
         return unavailable;
     }
 
-    public void setUnavailable(Integer unavailable) {
+    public void setUnavailable(Double unavailable) {
         this.unavailable = unavailable;
     }
 
-    public Integer getBackOrdered() {
+    public Double getUnavailableFromSales() {
+        return unavailableFromSales;
+    }
+
+    public void setUnavailableFromSales(Double unavailableFromSales) {
+        this.unavailableFromSales = unavailableFromSales;
+    }
+
+    public Double getUnavailableFromSample() {
+        return unavailableFromSample;
+    }
+
+    public void setUnavailableFromSample(Double unavailableFromSample) {
+        this.unavailableFromSample = unavailableFromSample;
+    }
+
+    public Double getUnavailableFromLoan() {
+        return unavailableFromLoan;
+    }
+
+    public void setUnavailableFromLoan(Double unavailableFromLoan) {
+        this.unavailableFromLoan = unavailableFromLoan;
+    }
+
+    public Double getBackOrdered() {
         return backOrdered;
     }
 
-    public void setBackOrdered(Integer backOrdered) {
+    public void setBackOrdered(Double backOrdered) {
         this.backOrdered = backOrdered;
     }
 
-    public Integer getDropShip() {
+    public Double getDropShip() {
         return dropShip;
     }
 
-    public void setDropShip(Integer dropShip) {
+    public void setDropShip(Double dropShip) {
         this.dropShip = dropShip;
     }
 
-    public Integer getAvailableForSale() {
+    public Double getAvailableForSale() {
         return availableForSale;
     }
 
-    public void setAvailableForSale(Integer availableForSale) {
+    public void setAvailableForSale(Double availableForSale) {
         this.availableForSale = availableForSale;
     }
 
-    public Integer getAvailableToPick() {
+    public Double getAvailableToPick() {
         return availableToPick;
     }
 
-    public void setAvailableToPick(Integer availableToPick) {
+    public void setAvailableToPick(Double availableToPick) {
         this.availableToPick = availableToPick;
+    }
+
+    public String getModifiedDate() {
+        return modifiedDate;
+    }
+
+    public void setModifiedDate(String modifiedDate) {
+        this.modifiedDate = modifiedDate;
     }
 }
